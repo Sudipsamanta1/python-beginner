b57614be187ab3ce6fdf50f2565875b9957ea950
Sudipsamanta1 <54319964+Sudipsamanta1@users.noreply.github.com>
2019-09-24 13:59:43 +0530

    Add files via upload

---
+++ separate-space.py
@@ -0,0 +1,6 @@
+
+input_string = input("Enter the string separated by space ")
+string_lst  = input_string.split(" ")
+print("Printing the string")
+print(string_lst)
+
---
+++ string-split.py
@@ -0,0 +1,5 @@
+A="I am on Fire"
+print(A)
+print('\n')
+B=A.split(" ")
+print(B)
